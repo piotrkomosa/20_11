0d1d5768cec919e80ef4325a20739fa861e73638
Piotr Komosa <komosa.piotr@gmail.com>
2019-11-21 23:37:42 +0100

    commit4

--- src/Logic.java
+++ src/Logic.java
@@ -1,34 +1,52 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+
 
 public class Logic {
     public static final String endOfProgram = "STOP";
     public static Scanner scanner = new Scanner(System.in);
 
-    static void addPlayers() {
-        ArrayList<Player> players = new ArrayList<>();
+
+    public static void addPlayers() throws IOException {
+        List<Player> players = new ArrayList<>();
         System.out.println("Podaj wynik kolejnego gracza (lub STOP):");
         String inputUser = scanner.nextLine();
-        do {
-            Player player = new Player();
+        while (!inputUser.equals(endOfProgram)) {
             System.out.println("podaj imie");
-            player.setFirstName(scanner.nextLine());
+            String firstName = scanner.nextLine();
             System.out.println("podaj nazwisko");
-            player.setLastName(scanner.nextLine());
+            String lastName = scanner.nextLine();
             System.out.println("podaj wynik");
-            player.setScore(scanner.nextDouble());
-            players.add(new Player(scanner.nextLine(), scanner.nextLine(), scanner.nextDouble()));
-            System.out.println(player.getFirstName()+" "+player.getLastName()+";" +player.getScore());
-        }
-        while (inputUser.equals(endOfProgram));
-        System.out.println("Koniec programu");
+            Double score = scanner.nextDouble();
+            Player player = new Player(firstName, lastName, score);
+            System.out.println("Podaj wynik kolejnego gracza (lub STOP):");
+       } System.out.println("Koniec programu");
+        sortPlayers(players);
+        writeToFile(players, "stats.csv");
     }
-    private static void sortPlayers(ArrayList<Player> players) {
+    private static void sortPlayers(List<Player> players) {
         for (Player p : players) {
             PlayerComparator playerComparator = new PlayerComparator();
             Collections.sort(players, playerComparator);
         }
     }
+
+    static void writeToFile(List<Player> player, String fileName) throws IOException {
+        String inputUser = scanner.nextLine();
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName));
+        while (inputUser.equals(endOfProgram)) {
+            bufferedWriter.write(player.toString());
+
+        }
+        bufferedWriter.close();
+    }
 }
+
+
+
+
--- src/Results.java
+++ src/Results.java
@@ -1,8 +1,9 @@
+import java.io.IOException;
 
 
 public class Results {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         Logic.addPlayers();
     }
